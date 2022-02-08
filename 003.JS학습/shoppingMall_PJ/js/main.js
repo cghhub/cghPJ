a3c6e8510dda25fc6482ad5388dc5fbe37f3829f
cghHUB <kinglion2013@naver.com>
2022-02-08 12:20:39 +0900

    ㅇㅇㅇ

--- 003.JS학습/shoppingMall_PJ/js/main.js
+++ 003.JS학습/shoppingMall_PJ/js/main.js
@@ -18,12 +18,18 @@ window.addEventListener("load",() => {
         -> 이동시 한계값은 첫번째 슬라이드의 위치값은 0을 기준한다
         (3) 처음이전과 끝 다음 슬라이드는 처음과 마지막 한계값에서 연결하여
         구현한다.(처음은 마지막으로 마지막은 처음으로 돌아가게 함)
+        5.추가기능 : 슬라이드 위치표시 블릿
+        -블릿대상 : .indic
+        -변경내용 : 슬라이드 순번과 같은 순번의 li에 클래스 "on"주기
+        (나머지 빼기-초기화)
     *****************************************************************************/
 
         // 이벤트 대상 : .abtn
         let abtn = document.querySelectorAll(".abtn");
         // 변경 대상 : #slide
         let slide = document.querySelector("#slide")
+        // 추가 대상 : .indic li
+        let indic = document.querySelectorAll(".indic li");
 
         // 오른쪽버튼 클릭시
         abtn[1].onclick = ()=>goSlide(1); 
@@ -36,7 +42,7 @@ window.addEventListener("load",() => {
         // 슬라이드 번호 전역변수
         let snum = 0;
         // 슬라이드 개수 구해오기
-        const scnt = document.
+        const scnt = document.querySelectorAll('#slide li').length;
 
         /*************************************************** 
             함수명 : goSlide
@@ -60,12 +66,21 @@ window.addEventListener("load",() => {
                 // 슬라이드 번호 1씩감소
                 snum--;
                 // 한계수에서 마지막번호로!
-                if(snum===-1) snum=4;
-            }
+                if(snum===-1) snum=scnt-1;
+                // 마지막 슬라이드번호는 (슬라이드개수-1)
+            } //// else ///////////////
 
-            // 이동하기 : 슬라이드의 left값을 변경함
-            slide.style.left = "-100%";
+            // console.log("슬번호:",snum);
+
+            // 2.이동하기 : 슬라이드의 left값을 변경함
+            slide.style.left = (-100*snum)+"%";
             slide.style.transition = "left .4s ease-in-out";
+
+            // 3.블릿표시 변경하기
+            // (1) 초기화
+            for(let x of indic) x.classList.remove("on");
+            // (2) 적용하기 : 해당순번의 li에 class "on"넣기
+            indic[snum].classList.add("on");
 
             // a요소 기본이동막!
             return false;
